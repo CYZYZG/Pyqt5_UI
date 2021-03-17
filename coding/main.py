3be784b39806e244cac8e538c29123956a054ad5
cyzz <2550816610@qq.com>
2021-03-17 22:23:57 +0800

    Update main.py

--- coding/main.py
+++ coding/main.py
@@ -8,13 +8,15 @@ from PyQt5.QtCore import Qt
 import qtawesome
 from PyQt5 import QtCore, QtGui, QtWidgets
 <<<<<<< HEAD
-USER_PWD = { 'la_vie': 'password' }
 =======
 USER_PWD = {
         'la_vie': 'password',
         'la': 'pass'
     }
 >>>>>>> c355e97f673650400607f91818ffed222aafc5a8
+
+USER_PWD = { 'la_vie': 'password' }
+
 
 
 class MainUi(QtWidgets. QMainWindow):
